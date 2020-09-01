141105e629a7852d35f57623a805ba6faeedb14e
Filipe Limberger <lipilimb07@gmail.com>
2020-09-01 12:54:01 -0300

    Updates

---
+++ precache-manifest.3eb3d7a59a9fb07a3e2c816124aa00de.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "fafe8d710861c9f66044",
+    "url": "/open-source-gis-infographic/static/css/main.67e46357.chunk.css"
+  },
+  {
+    "revision": "fafe8d710861c9f66044",
+    "url": "/open-source-gis-infographic/static/js/main.fafe8d71.chunk.js"
+  },
+  {
+    "revision": "6198728e60ecd7e88c17",
+    "url": "/open-source-gis-infographic/static/js/runtime~main.6198728e.js"
+  },
+  {
+    "revision": "9cc5160a9a1cce20418d",
+    "url": "/open-source-gis-infographic/static/js/2.9cc5160a.chunk.js"
+  },
+  {
+    "revision": "107538141511e914f7b0c9586b488bb6",
+    "url": "/open-source-gis-infographic/static/media/Right-Tail.10753814.svg"
+  },
+  {
+    "revision": "7f09dcf81439018afaeddc1a68cbd4f1",
+    "url": "/open-source-gis-infographic/static/media/Left-Snake.7f09dcf8.svg"
+  },
+  {
+    "revision": "c57a708bc84bea17a52e8b659775f658",
+    "url": "/open-source-gis-infographic/static/media/Right-Snake.c57a708b.svg"
+  },
+  {
+    "revision": "4baba7df5fbd814bd366f30365742447",
+    "url": "/open-source-gis-infographic/static/media/Left-Head.4baba7df.svg"
+  },
+  {
+    "revision": "b01fa118a340b180f68f1fb708b6cd4b",
+    "url": "/open-source-gis-infographic/static/media/Right-Head.b01fa118.svg"
+  },
+  {
+    "revision": "f55e6e6014f6e1692edbb46a156d65b6",
+    "url": "/open-source-gis-infographic/static/media/Acumin-BdPro.f55e6e60.otf"
+  },
+  {
+    "revision": "bdb37477b68d098317daaf939f295527",
+    "url": "/open-source-gis-infographic/static/media/Acumin-RPro.bdb37477.otf"
+  },
+  {
+    "revision": "d3085f686df272f9e1a267cc69b2d24f",
+    "url": "/open-source-gis-infographic/static/media/Montserrat-Bold.d3085f68.ttf"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/open-source-gis-infographic/static/js/2.9cc5160a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9085252a9ffde8c52583b278dffe01eb",
+    "url": "/open-source-gis-infographic/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/open-source-gis-infographic/precache-manifest.3eb3d7a59a9fb07a3e2c816124aa00de.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/open-source-gis-infographic/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
