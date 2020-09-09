1e39e3ca4fc88ac4875bb8554f4a41f53ca33967
Filipe Limberger <lipilimb07@gmail.com>
2020-09-09 17:17:42 -0300

    Name typo

--- src/features/home/Snake.js
+++ src/features/home/Snake.js
@@ -5,18 +5,18 @@ export default class Snake extends Component {
 
   render() {
     const { first, side, last } = this.props;
-    const snakeTail = require(`../../images/Right-Tail.svg`);
+    const snakeTail = require(`../../images/Right-Snake-Tail.svg`);
     const snakeBody = require(`../../images/${side !== 'right' ? 'Right' : 'Left'}-Snake.svg`);
-    const snakeHead = require(`../../images/${side !== 'right' ? 'Right' : 'Left'}-Head.svg`);
+    const snakeHead = require(`../../images/${side !== 'right' ? 'Right' : 'Left'}-Snake-Head.svg`);
     return (
       <Fragment>
         {first ? (
           <div style={{ backgroundImage: `url(${snakeTail}` }} className="snake-tail" />
-        ) : null}
-        <div style={{ backgroundImage: `url(${snakeBody}` }} className={`snake-body ${side}`} />
-        {last ? (
+        ) : last ? (
           <div style={{ backgroundImage: `url(${snakeHead}` }} className={`snake-head ${side}`} />
-        ) : null}
+        ) : (
+          <div style={{ backgroundImage: `url(${snakeBody}` }} className={`snake-body ${side}`} />
+        )}
       </Fragment>
     );
   }
