4ca9eb60b648fee7bf7e25d43e141bcf616d6bb7
Filipe Limberger <lipilimb07@gmail.com>
2020-09-10 15:53:38 -0300

    Updates

--- precache-manifest.f14c6473b48a5810226cca24e5a88d59.js
+++ precache-manifest.f14c6473b48a5810226cca24e5a88d59.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = [
   {
-    "revision": "e26882e4dd4b67f03a96",
-    "url": "/open-source-gis-infographic/static/css/main.1c472b57.chunk.css"
+    "revision": "9d0fe71dc65d4de77f89",
+    "url": "/open-source-gis-infographic/static/css/main.17bdc314.chunk.css"
   },
   {
-    "revision": "e26882e4dd4b67f03a96",
-    "url": "/open-source-gis-infographic/static/js/main.e26882e4.chunk.js"
+    "revision": "9d0fe71dc65d4de77f89",
+    "url": "/open-source-gis-infographic/static/js/main.9d0fe71d.chunk.js"
   },
   {
     "revision": "6198728e60ecd7e88c17",
     "url": "/open-source-gis-infographic/static/js/runtime~main.6198728e.js"
   },
   {
-    "revision": "9cc5160a9a1cce20418d",
-    "url": "/open-source-gis-infographic/static/js/2.9cc5160a.chunk.js"
+    "revision": "ebce9999b10d845ba8fb",
+    "url": "/open-source-gis-infographic/static/js/2.ebce9999.chunk.js"
   },
   {
     "revision": "ebf1b7c3ce667302c81e3907e36bdaaf",
     "url": "/open-source-gis-infographic/static/media/Right-Snake-Tail.ebf1b7c3.svg"
   },
   {
-    "revision": "60ed3bb6c0526a958e0f90ea071485c8",
-    "url": "/open-source-gis-infographic/static/media/Left-Snake.60ed3bb6.svg"
+    "revision": "8edbbf9ae3b6a0a0457a65bbddeab63d",
+    "url": "/open-source-gis-infographic/static/media/Loop-Snake.8edbbf9a.svg"
   },
   {
-    "revision": "b7027f812719a92fb25bf190e18745b9",
-    "url": "/open-source-gis-infographic/static/media/Right-Snake.b7027f81.svg"
+    "revision": "60ed3bb6c0526a958e0f90ea071485c8",
+    "url": "/open-source-gis-infographic/static/media/Right-Snake.60ed3bb6.svg"
   },
   {
     "revision": "09cdd625f2dfd3ba32fd86417d840ee1",
@@ -36,10 +36,6 @@ self.__precacheManifest = [
     "url": "/open-source-gis-infographic/static/media/Right-Snake-Head.41545645.svg"
   },
   {
-    "revision": "bdb37477b68d098317daaf939f295527",
-    "url": "/open-source-gis-infographic/static/media/Acumin-RPro.bdb37477.otf"
-  },
-  {
     "revision": "d3085f686df272f9e1a267cc69b2d24f",
     "url": "/open-source-gis-infographic/static/media/Montserrat-Bold.d3085f68.ttf"
   },
@@ -48,11 +44,15 @@ self.__precacheManifest = [
     "url": "/open-source-gis-infographic/static/media/Acumin-BdPro.f55e6e60.otf"
   },
   {
-    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
-    "url": "/open-source-gis-infographic/static/js/2.9cc5160a.chunk.js.LICENSE.txt"
+    "revision": "bdb37477b68d098317daaf939f295527",
+    "url": "/open-source-gis-infographic/static/media/Acumin-RPro.bdb37477.otf"
   },
   {
-    "revision": "c55309efa92edf438afe288548eeec28",
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/open-source-gis-infographic/static/js/2.ebce9999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c1d3b8f0732c59ce98a7258634728f",
     "url": "/open-source-gis-infographic/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/open-source-gis-infographic/precache-manifest.c95e9e69ed903160368df4e24a027652.js"
+  "/open-source-gis-infographic/precache-manifest.f14c6473b48a5810226cca24e5a88d59.js"
 );
 
 workbox.clientsClaim();
