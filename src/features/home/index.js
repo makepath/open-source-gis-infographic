3bb2aecf2f56b5f9055bd8d0da67779e863d5338
Filipe Limberger <lipilimb07@gmail.com>
2021-06-14 14:43:41 -0300

    Revert "add filter component"
    This reverts commit 2a179e904bbcbeca537ab84f41f04025d28a55cd.

--- src/features/home/index.js
+++ src/features/home/index.js
@@ -4,4 +4,3 @@ export { default as Tool } from './Tool';
 export { default as Snake } from './Snake';
 export { default as SnakeHead } from './SnakeHead';
 export { default as ToolPopup } from './ToolPopup';
-export { default as Filters } from './Filters';
