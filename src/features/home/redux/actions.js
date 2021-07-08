1b8d957222982ee912b58c72f9daaf4ebbb19d88
Filipe Limberger <lipilimb07@gmail.com>
2021-07-08 18:17:50 -0300

    Squashed commit of the following:
    commit 9f336570429920ec8481fe3940da9630ca17d770
    Author: Filipe Limberger <lipilimb07@gmail.com>
    Date:   Thu Jul 8 18:12:14 2021 -0300
    
        fix css for small devices
    
    commit 49ffb2f2e239984a4a364fcd128fecc228e3b1e6
    Author: Filipe Limberger <lipilimb07@gmail.com>
    Date:   Mon Jun 14 14:24:22 2021 -0300
    
        search filter

---
+++ src/features/home/redux/actions.js
@@ -0,0 +1,2 @@
+export { loadTools, filterTools } from './dataManagement';
+export { openPopup, closePopup, setSearch, setCategory, setFileFormat } from './userActions';
---
+++ src/features/home/redux/constants.js
@@ -0,0 +1,7 @@
+export const LOAD_TOOLS = 'LOAD_TOOLS';
+export const FILTER_TOOLS = 'FILTER_TOOLS';
+export const OPEN_POPUP = 'OPEN_POPUP';
+export const CLOSE_POPUP = 'CLOSE_POPUP';
+export const SET_SEARCH = 'SET_SEARCH';
+export const SET_CATEGORY = 'SET_CATEGORY';
+export const SET_FILE_FORMAT = 'SET_FILE_FORMAT';
